bae95659d99d2326070e4f84219ee4552a9a6331
James Yeates <jimibue@gmail.com>
2021-09-08 10:45:12 -0600

    bug fix

--- client/src/index.js
+++ client/src/index.js
@@ -4,6 +4,7 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import "semantic-ui-css/semantic.min.css";
+import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.render(
   <BrowserRouter>
