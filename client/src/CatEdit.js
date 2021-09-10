9857900426339e535dcb01088f5180409e1d74fc
James Yeates <jimibue@gmail.com>
2021-09-10 12:10:56 -0600

    update cat working

--- client/src/CatEdit.js
+++ client/src/CatEdit.js
@@ -1,12 +1,31 @@
-import React from "react";
+import axios from "axios";
+import React, { useState } from "react";
+import { Button, Form } from "semantic-ui-react";
 
 // static page...
 const CatEdit = (props) => {
+  // will cause error if you refresh on edit page
+  const [name, setName] = useState(props.location.cat.name);
   const id = props.match.params.id;
+
+  const handleSubmit = async () => {
+    await axios.put(`/api/cats/${id}`, { name });
+    props.history.push("/cats");
+  };
   return (
     <div>
       <h1>CatEdit</h1>
       <p>{id}</p>
+      <Form onSubmit={handleSubmit}>
+        <Form.Field>
+          <label>name</label>
+          <Form.Input
+            value={name}
+            onChange={(e, { value }) => setName(value)}
+          />
+          <Button type="submit">add</Button>
+        </Form.Field>
+      </Form>
     </div>
   );
 };
--- client/src/CatNew.js
+++ client/src/CatNew.js
@@ -1,10 +1,27 @@
-import React from "react";
+import axios from "axios";
+import React, { useState } from "react";
+import { Button, Form } from "semantic-ui-react";
 
 // static page...
-const CatNew = () => {
+const CatNew = (props) => {
+  const [name, setName] = useState("");
+  const handleSubmit = async () => {
+    await axios.post("/api/cats", { name });
+    props.history.push("/cats");
+  };
   return (
     <div>
       <h1>CatNew</h1>
+      <Form onSubmit={handleSubmit}>
+        <Form.Field>
+          <label>name</label>
+          <Form.Input
+            value={name}
+            onChange={(e, { value }) => setName(value)}
+          />
+          <Button type="submit">add</Button>
+        </Form.Field>
+      </Form>
     </div>
   );
 };
